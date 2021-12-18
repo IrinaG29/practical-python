68c892d90f7f3cd58be591e49b0980f6c40c31ae
IrinaG29 <36776772+IrinaG29@users.noreply.github.com>
2021-12-18 23:22:16 +0000

    solution exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
-# bounce.py
-#
 # Exercise 1.5
+
+height = 100
+counter = 1
+
+while counter < 11:
+    bounce = (3 * height) / 5
+    height = bounce
+    print(counter, round(bounce, 4))
+    counter = counter + 1
